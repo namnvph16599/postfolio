9a4dfc11267a961258429b0b6569f33dc0f4735e
Ngô Vưn Nam <anh02284@gmail.com>
2022-04-13 16:46:52 +0700

    change : skill

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,6 @@ const Header = ({ info }) => {
         var btnContainer = document.getElementById("navbar");
         var btns = btnContainer.getElementsByClassName("nav-link");
 
-        // Loop through the buttons and add the active class to the current/clicked button
         for (var i = 0; i < btns.length; i++) {
             btns[i].addEventListener("click", function () {
                 var current = document.getElementsByClassName("active");
--- src/pages/Skill.jsx
+++ src/pages/Skill.jsx
@@ -3,34 +3,64 @@ import React, { useEffect, useState } from 'react'
 const Skill = ({ skill }) => {
   // console.log(skill);
   return (
-    <div><section id="skills" className="skills section-bg">
-      <div className="container">
+    <div>
+      <section id="skills" className="skills section-bg">
+        <div className="container">
 
-        <div className="section-title">
-          <h2>Skills</h2>
-          {/* <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p> */}
-        </div>
+          <div className="section-title">
+            <h2>Skills</h2>
+            {/* <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p> */}
+          </div>
 
-        {/* <div className="row skills-content"> */}
-
-        {/* <div className="col-lg-6" data-aos="fade-up"> */}
-
-        {skill?.map((val, ind) => (
-          <div className="progress" key={ind}>
-            <span className="skill">{val.skillName} <i className="val">{val.skillProgress} %</i></span>
-            <div className="progress-bar-wrap">
-              <div className="progress-bar" style={{
-                width: `${val.skillProgress}%`
-              }} role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+          <div className="col-lg-12 pt-4 pt-lg-0 content" >
+            <div className="row">
+              {skill?.map(item => (
+              <div className="col-lg-6" key={item._id}>
+                <h3>{item.skillName}</h3>
+                <ul>
+                  <li><i className="bi bi-chevron-right"></i> <strong>{item.skillProgress}</strong></li>
+                </ul>
+              </div>
+              ))}
+              {/* <div className="col-lg-6">
+                <h3>Web framework</h3>
+                <ul>
+                  <li><i className="bi bi-chevron-right"></i> <strong>Express</strong></li>
+                </ul>
+              </div>
+              <div className="col-lg-6">
+                <h3>Library</h3>
+                <ul>
+                  <li><i className="bi bi-chevron-right"></i> <strong>ReactJs, Redux, Mongoose, Redux Toolkit</strong></li>
+                </ul>
+              </div>
+              <div className="col-lg-6">
+                <h3>SCM</h3>
+                <ul>
+                  <li><i className="bi bi-chevron-right"></i> <strong>Git, Github ,GitLab</strong></li>
+                </ul>
+              </div>
+              <div className="col-lg-6">
+                <h3>Database</h3>
+                <ul>
+                  <li><i className="bi bi-chevron-right"></i> <strong>SQL Sever, MySQL, MongoDB</strong></li>
+                </ul>
+              </div>
+              <div className="col-lg-6">
+                <h3>JavaScript Tool</h3>
+                <ul>
+                  <li><i className="bi bi-chevron-right"></i> <strong>ESLint ,Thunder Client, Postman</strong></li>
+                </ul>
+              </div> */}
             </div>
           </div>
-        )
-        )}
 
 
-        {/* </div> */}
 
-        {/* <div className="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+
+          {/* </div> */}
+
+          {/* <div className="col-lg-6" data-aos="fade-up" data-aos-delay="100">
 
           <div className="progress">
             <span className="skill">PHP <i className="val">80%</i></span>
@@ -55,10 +85,10 @@ const Skill = ({ skill }) => {
 
         </div> */}
 
-        {/* </div> */}
+        </div >
 
-      </div>
-    </section></div>
+      </section >
+    </div >
   )
 }
 
