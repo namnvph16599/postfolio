19ac44b6a280d437b4ecb29a85a1187524d06843
Ngô Vưn Nam <anh02284@gmail.com>
2022-04-13 15:35:36 +0700

    change : api instance

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,7 +21,7 @@ const Header = ({ info }) => {
                     <ul>
                         <li><a href="#hero" className="nav-link scrollto active"><i className="bx bx-home"></i> <span>Home</span></a></li>
                         <li><a href="#about" className="nav-link scrollto"><i className="bx bx-user"></i> <span>About</span></a></li>
-                        <li><a href="#skills" className="nav-link scrollto"><i className="bx bx-user"></i> <span>Skills</span></a></li>
+                        <li><a href="#skills" className="nav-link scrollto"><i className="bx bx-server"></i> <span>Skills</span></a></li>
                         <li><a href="#softskill" className="nav-link scrollto"><i className="bx bx-server"></i> <span>Soft Skill</span></a></li>
                         <li><a href="#resume" className="nav-link scrollto"><i className="bx bx-file-blank"></i> <span>Resume</span></a></li>
                         <li><a href="#portfolio" className="nav-link scrollto"><i className="bx bx-book-content"></i> <span>Project</span></a></li>
