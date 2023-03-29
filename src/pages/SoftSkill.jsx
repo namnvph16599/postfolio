354b65d7ada833430063b05b169008012fb91af4
Ngô Vưn Nam <anh02284@gmail.com>
2023-03-29 22:29:33 +0700

    fix: title  soft skills

--- src/pages/SoftSkill.jsx
+++ src/pages/SoftSkill.jsx
@@ -6,7 +6,7 @@ const SoftSkill = ({ softSkill }) => {
             <div className="container">
 
                 <div className="section-title">
-                    <h2>Soft skill</h2>
+                    <h2>Soft Skills</h2>
                     {/* <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p> */}
                 </div>
                 <div className="row">
