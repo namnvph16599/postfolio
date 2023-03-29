28dc346cf5a4be06807b9784ddb88327f79b2733
Ngô Vưn Nam <anh02284@gmail.com>
2023-03-29 22:23:13 +0700

    fix: change skills

--- src/pages/Skill.jsx
+++ src/pages/Skill.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
-const Skill = ({ skill }) => {
-  // console.log(skill);
+const Skill = () => {
   return (
     <div>
       <section id="skills" className="skills section-bg">
@@ -9,82 +8,36 @@ const Skill = ({ skill }) => {
 
           <div className="section-title">
             <h2>Skills</h2>
-            {/* <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p> */}
           </div>
 
           <div className="col-lg-12 pt-4 pt-lg-0 content" >
             <div className="row">
-              {skill?.map(item => (
-              <div className="col-lg-6" key={item._id}>
-                <h5>{item.skillName}</h5>
+              <div className="col-lg-6">
+                <h5>Frontend</h5>
                 <ul>
-                  <li><i className="bi bi-chevron-right"></i> <strong>{item.skillProgress}</strong></li>
-                </ul>
-              </div>
-              ))}
-              {/* <div className="col-lg-6">
-                <h3>Web framework</h3>
-                <ul>
-                  <li><i className="bi bi-chevron-right"></i> <strong>Express</strong></li>
+                  <li><i className="bi bi-chevron-right"></i> <strong>HTML, CSS, JAVASCRIPT</strong></li>
+                  <li><i className="bi bi-chevron-right"></i> <strong>REACTJS, NEXTJS</strong></li>
+                  <li><i className="bi bi-chevron-right"></i> <strong>REDUX, REDUX TOOLKIT, REDUX THUNK</strong></li>
+                  <li><i className="bi bi-chevron-right"></i> <strong>TAILWIND, ANT DESIGN</strong></li>
                 </ul>
               </div>
               <div className="col-lg-6">
-                <h3>Library</h3>
+                <h5>Backend</h5>
                 <ul>
-                  <li><i className="bi bi-chevron-right"></i> <strong>ReactJs, Redux, Mongoose, Redux Toolkit</strong></li>
+                  <li><i className="bi bi-chevron-right"></i> <strong>NODEJS, EXPRESSJS</strong></li>
+                  <li><i className="bi bi-chevron-right"></i> <strong>MYSQL, MONGODB, MONGOOSE</strong></li>
+                  <li><i className="bi bi-chevron-right"></i> <strong>JWT</strong></li>
                 </ul>
               </div>
               <div className="col-lg-6">
-                <h3>SCM</h3>
+                <h5>Orther</h5>
                 <ul>
-                  <li><i className="bi bi-chevron-right"></i> <strong>Git, Github ,GitLab</strong></li>
+                  <li><i className="bi bi-chevron-right"></i> <strong>GIT, FIGMA, POSTMAN</strong></li>
                 </ul>
               </div>
-              <div className="col-lg-6">
-                <h3>Database</h3>
-                <ul>
-                  <li><i className="bi bi-chevron-right"></i> <strong>SQL Sever, MySQL, MongoDB</strong></li>
-                </ul>
-              </div>
-              <div className="col-lg-6">
-                <h3>JavaScript Tool</h3>
-                <ul>
-                  <li><i className="bi bi-chevron-right"></i> <strong>ESLint ,Thunder Client, Postman</strong></li>
-                </ul>
-              </div> */}
+
             </div>
           </div>
-
-
-
-
-          {/* </div> */}
-
-          {/* <div className="col-lg-6" data-aos="fade-up" data-aos-delay="100">
-
-          <div className="progress">
-            <span className="skill">PHP <i className="val">80%</i></span>
-            <div className="progress-bar-wrap">
-              <div className="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-          </div>
-
-          <div className="progress">
-            <span className="skill">WordPress/CMS <i className="val">90%</i></span>
-            <div className="progress-bar-wrap">
-              <div className="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-          </div>
-
-          <div className="progress">
-            <span className="skill">Photoshop <i className="val">55%</i></span>
-            <div className="progress-bar-wrap">
-              <div className="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-          </div>
-
-        </div> */}
-
         </div >
 
       </section >
